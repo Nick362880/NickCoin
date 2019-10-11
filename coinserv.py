8bc94dbbdb87226ac6e9004bc60871f41b69dc97
Nick <nickelg25@gmail.com>
2019-10-11 15:00:30 -0400

    Added functionality for user search

--- coinserv.py
+++ coinserv.py
@@ -1,28 +1,21 @@
 import asyncio
 import websockets
 import os.path
-import re
-import random
 
 async def echo(websocket, path):
 	async for message in websocket:
 		print("--- Incoming Connection:", websocket.remote_address[0], "---")
 		print("    Message:", message, end="\n\n")
-		if message.split(" ")[0] == "create":
-			with open("users/" + message.split(" ")[1], "w+") as f:
-				f.write(message.split(" ")[2] + "\n")
-		elif message.split(" ")[0] == "query":
-			# len([re.findall("\:(.*" + message.split(" ")[1] + ")\\s", line) for line in open("coinids")]) >= 1;
-			if (random.randint(0, 5)):
-				await websocket.send("1");
+		proc = message.split(" ")[0]
+
+		# Tells whether a certain user has registered
+		if proc == "search":
+			if os.path.isfile("users/" + message.split(" ")[1]):
+				await websocket.send("true")
 			else:
-				await websocket.send("0");
-		else: # (?<=\:)(.*?)(?=\\s)
-			if os.path.exists("users/" + message.split(" ")[0]):
-				await websocket.send("1")
-			else:
-				await websocket.send("0")
+				await websocket.send("false");
+		print("");
 
 asyncio.get_event_loop().run_until_complete(
-	websockets.serve(echo, "localhost", 5678))
+	websockets.serve(echo, "localhost", 3280))
 asyncio.get_event_loop().run_forever()
--- minecore.js
+++ minecore.js
@@ -1,6 +1,37 @@
-var submit
+var uname, psswd, submit;
 window.onload = function() {
+	// DOM setup
 	document.getElementById("hhelp").onmousedown = function() {
-		alert("Username: Must be 1 - 16 characters in length. Only numbers, letters, and _ permitted.\n\nPassword: Must be 0 (no password) - 16 characters in length. All characters permitted other than :, /, and \\.");
+		alert("Username: Must be 1 - 16 characters in length. Only numbers, letters, and _ permitted.\n\nPassword: Must be 0 (no password) - 16 characters in length. All characters permitted other than /, \\, and :.");
+	}
+	
+	// Element setup
+	uname = document.getElementById("uname");
+	psswd = document.getElementById("psswd");
+	submit = document.getElementById("sconf");
+	
+	// Submit button functionality
+	submit.onclick = function() {
+		var u = uname.value;
+		var p = psswd.value;
+		// Checks username validity
+		if (((u.length >= 1 && u.length <= 16) && u.match(/^[A-Za-z0-9_]+$/g)) && ((p.length >= 0 && p.length <= 16) && !p.match(/^[:\/\\]+$/g))) {
+			// Perform user search
+			var status;
+			var ws = new WebSocket("ws://localhost:3280");
+			ws.onopen = function() {
+				ws.send("search " + u);
+			}
+			ws.onmessage = function(e) {
+				ws.close(1000);
+				if (e.data == "true") {
+					alert("signing in");
+				} else {
+					confirm("account not in existence. Create new account?")
+				}
+			}
+		} else {
+			alert("Invalid username or password");
+		}
 	}
 }
